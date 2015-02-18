f223d119a77bac5a60e7d187a007b1267a504fcf
Justin Herrick <justin@justinherrick.com>
2015-02-18 11:22:00 -0600

    adding association and seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#user, pets, food
+
+
+10.times do
+  user = User.create(first_name: Faker::Name.first_name,
+                     last_name: Faker::Name.last_name,
+                     age:rand(18...30),
+                     favorite_color: %w(red blue yellow brown green orange).sample)
+  3.times do
+    user.meals.create(name: Faker::Hacker.noun,
+                      cost: rand(5..99))
+  end
+
+  2.times do
+    user.pets.create(name: %w(fido fluffy foobar ruby rm-rf git lilbuck beans iron mittens roofus).sample,
+                     kind: %w(fish dog cat bird turtle vampire-bat rabbit).sample,
+                     weight: rand(14..250)) #oz
+  end
+end
